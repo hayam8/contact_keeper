5fd74fcebdab31f2ad88a2131648f79c768b52ab
hayam8 <hayam8@uw.edu>
2019-09-18 20:10:11 -0700

    User model & validation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password must be 8 or more characters").isLength({
+      min: 8
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    // returns bad request and json object of errors in an array
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("passed");
+  }
+);
 
 // export the router
 module.exports = router;
