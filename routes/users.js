9b09a9ed1e7d35f9c6aeecb81e32b4b66b772416
hayam8 <hayam8@uw.edu>
2019-09-18 20:19:39 -0700

    Hash passwords & Register route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
@@ -18,14 +19,43 @@ router.post(
       min: 8
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     // returns bad request and json object of errors in an array
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("passed");
+    const { name, email, password } = req.body;
+
+    // check for errors duplicate name / email
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      // Create new instance of user with required fields
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // Generate salt to use for hashing password
+      const salt = await bcrypt.genSalt(10);
+
+      // Hash password
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
